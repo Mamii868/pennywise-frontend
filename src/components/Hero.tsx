d5c23a1b9e8e183e902a38eb1489555a9206383a
Mamii868 <marcmotenii@gmail.com>
2026-07-16 02:39:02 -0500

    Add better title css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,9 @@ const Hero = ({ currentUser, onSignIn, onSignUp, onLogout }: HeroProps) => {
   return (
     <section className="hero flex min-h-96 w-full items-center justify-center px-6 py-16">
       <div className="titleContainer flex flex-col items-center justify-center gap-4 text-center">
-        <h1 className="text-4xl font-bold">Penny Wise</h1>
+        <h1 className="text-clown-red text-6xl font-semibold tracking-[0.2em] uppercase">
+          Penny Wise
+        </h1>
         <p className="text-lg text-center">Dangerously smart money management</p>
         {currentUser ? (
           <>

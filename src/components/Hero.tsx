4dda8e74069eb3c5528bb0acecaf606891d42023
Mamii868 <marcmotenii@gmail.com>
2026-07-16 13:36:31 -0500

    Add icon

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import Button from "./Button";
 import type { User } from "../types/userTypes";
+import balloon from "../assets/balloon.svg";
 
 type HeroProps = {
   currentUser: User | null;
@@ -12,13 +13,25 @@ const Hero = ({ currentUser, onSignIn, onSignUp, onLogout }: HeroProps) => {
   return (
     <section className="hero flex min-h-96 w-full items-center justify-center px-6 py-16">
       <div className="titleContainer flex flex-col items-center justify-center gap-4 text-center">
-        <h1 className="text-clown-red text-6xl font-semibold tracking-[0.2em] uppercase">
-          Penny Wise
-        </h1>
-        <p className="text-lg text-center">Dangerously smart money management</p>
+        <div className="logoContainer flex items-center justify-center gap-2">
+          <img
+            src={balloon}
+            alt="Penny Wise Logo"
+            className="h-12 w-12 rounded-full"
+          />
+          <h1 className="text-clown-red text-6xl font-semibold tracking-[0.2em] uppercase">
+            Penny Wise
+          </h1>
+        </div>
+
+        <p className="text-center text-lg">
+          Dangerously smart money management
+        </p>
         {currentUser ? (
           <>
-            <p className="text-dark-subtext">Signed in as {currentUser.username}</p>
+            <p className="text-dark-subtext">
+              Signed in as {currentUser.username}
+            </p>
             <Button onClick={onLogout} variant="outline">
               Sign out
             </Button>
